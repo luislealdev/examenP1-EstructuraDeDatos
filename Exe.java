6c9c4a48b409d991d12e1d694a46effad8412c33
Luis Leal <luisrrleal@gmail.com>
2022-09-08 23:02:49 -0500

    First commit, has an error to fix

---
+++ Exe.java
@@ -0,0 +1,69 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+public class Exe {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        try (
+                // CREAR ARCHIVO O LEERLO
+                RandomAccessFile v_archivo = new RandomAccessFile("./flotantes.bin", "rw")) {
+            // Metemos 10 flotantes aleatorios al archivo
+
+            v_archivo.seek(0);
+
+            int contador;
+            for (contador = 0; contador < 20; contador++) {
+                // Generamos flotantes aleatorios
+                float v_valor = (float) Math.random();
+                try {
+                    // Escribimos los flotantes
+                    v_archivo.writeFloat(v_valor);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("----------ARCHIVO FLOTANTES----------");
+            int contador2;
+            for (contador2 = 0; contador2 < 20; contador2++) {
+                float v_valor =  v_archivo.readFloat();
+                // Mostrar los flotantes
+                System.out.println(v_valor);
+            }
+
+            System.out.println("Ingrese el valor del flotante a cambiar.");
+            float v_floatACambiar = sc.nextFloat();
+
+            for (contador = 0; contador < 20; contador++) {
+                if (v_archivo.readFloat() == v_floatACambiar) {
+                    v_archivo.seek(contador * 8);
+                    v_archivo.writeFloat(v_floatACambiar);
+                    return;
+                } else {
+                    if (contador == 19) {
+                        System.out.println("No se ha encontrado el float a cambiar");
+                    }
+                }
+            }
+
+            System.out.println("----------ARCHIVO FLOTANTES----------");
+            for (contador = 0; contador < 20; contador++) {
+                // Mostrar los flotantes
+                System.out.println(v_archivo.readFloat());
+            }
+
+            try {
+                // CERRAR ARCHIVO
+                v_archivo.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
